d414c50c7175ea3a742eedd5184b820867bd77fc
nurulamin092 <mnurulamin092@gmial.com>
2022-10-08 12:57:56 +0600

    todo post

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const todoSchema = require('../schemas/todoSchema');
 const router = express.Router();
+const Todo = new mongoose.model("Todo",todoSchema);
 
 // get all the todoS
 router.get('/',async(req,res)=>{
@@ -11,13 +14,27 @@ router.get('/:id',async(req,res)=>{
 
 });
 
-//POST TODO
-router.post('/all ',async(req,res)=>{
+//POST A TODO
+router.post('/',async(req,res)=> {
+    const newTodo = new Todo (req.body);
+    await newTodo.save((err) => {
+        if (err) {
+            res.status(500).json({
+                error: "There was a server side error"
+            });
+        } else {
+            res.status(200).json({
+                message: "Todo was inserted successfully"
+            })
+
+        }
+
+    })
 
 });
 
 //POST MULTIPLE TODO-S
-router.post('/',async(req,res)=>{
+router.post('/all',async(req,res)=>{
 
 });
 
@@ -27,7 +44,6 @@ router.put('/:id',async(req,res)=>{
 });
 
 // DELETE TODO
-
 router.delete('/:id',async(req,res)=>{
 
 });
